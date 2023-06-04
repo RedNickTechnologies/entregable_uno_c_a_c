f9857d9426e264da4a124c8d32552b244e988f60
Nicolas <ngacevedo5@gmail.com>
2023-06-04 15:11:50 -0300

    13) colocando script comprar tickets

---
+++ script/comprar_tickets.js
@@ -0,0 +1,28 @@
+var botones = [];
+botones = document.getElementsByClassName('appendable');
+
+for(var i=0; i<botones.length ;i++) {
+
+   botones[i].addEventListener('click',append);
+}
+
+
+function append(event) {
+   var elemento = document.getElementById('calctext');
+   var valor = elemento.value;
+   elemento.value = valor + event.srcElement.innerHTML;
+}
+
+function operacion() {
+   var resultado = document.getElementById('resultado');
+   var valor = document.getElementById('calctext').value;
+   resultado.innerText = eval(valor);
+}
+
+const btnClick = document.getElementById('equals');
+btnClick.addEventListener('click',operacion);
+function operacion() {
+   var valor = document.getElementById('calctext');   
+   var elemento = valor.value;
+   valor.value = eval(elemento);
+}
